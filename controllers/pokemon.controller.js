a6f7242756f28944e32f5e0f3219f157e25e9017
ArielVelastegui <115893022+ArielVelastegui@users.noreply.github.com>
2023-02-05 21:11:54 -0500

    Add files via upload

---
+++ controllers/pokemon.controller.js
@@ -0,0 +1,138 @@
+const Pokemon = require('../models/pokemon.model');
+
+exports.findPokemons = async (req, res) => {
+  try {
+    const { count, rows } = await Pokemon.findAndCountAll({
+      attributes: ['id', 'name', 'image'],
+      where: {
+        status: 'available',
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      count,
+      results: rows,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: 'internal server error',
+    });
+  }
+};
+
+exports.findPokemon = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const pokemon = await Pokemon.findOne({
+      attributes: ['id', 'name', 'image'],
+      where: {
+        id,
+        status: 'available',
+      },
+    });
+
+    if (!pokemon) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Pokemon not found',
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      pokemon,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: 'internal server error',
+    });
+  }
+};
+
+exports.createPokemon = async (req, res) => {
+  try {
+    const { name, image } = req.body;
+
+    const pokemon = await Pokemon.create({ 
+        name:name.toLowerCase(), 
+        image 
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Pokemon created successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: 'internal server error',
+    });
+  }
+};
+
+exports.updatePokemon = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, image } = req.body;
+
+    const pokemon = await Pokemon.findOne({
+      where: {
+        id,
+        status: 'available',
+      },
+    });
+
+    if (!pokemon) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Pokemon not found',
+      });
+    }
+
+    await pokemon.update({ name, image });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Pokemon updated successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: 'internal server error',
+    });
+  }
+};
+
+exports.deletePokemon = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const pokemon = await Pokemon.findOne({
+        where: {
+            id,
+            status: 'available',
+        }
+    })
+if (!pokemon) {
+    return res.status(404).json({
+        status: 'error',
+        message: 'Pokemon not found',
+    })
+}
+
+await pokemon.update({ status: 'disabled' })
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Pokemon deleted successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: 'internal server error',
+    });
+  }
+};
